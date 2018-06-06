7154f3ed4bab4c6bbfd1463a1b54265e0bc6d2a0
Andre <andrep615@hotmail.com>
2018-06-06 20:04:02 +0300

    Color and square variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var colors = [
+  "rgb(235, 59, 90)",
+  "rgb(247, 183, 49)",
+  "rgb(32, 191, 107)",
+  "rgb(75, 123, 236)",
+  "rgb(165, 94, 234)",
+  "rgb(247, 241, 227)"
+]
+
+var squares = document.querySelectorAll(".square");
